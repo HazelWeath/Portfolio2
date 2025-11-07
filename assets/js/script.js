30cf077ed4d8c60223ab27086482034cd3a31b5d
HazelWeath <125269904+HazelWeath@users.noreply.github.com>
2025-11-07 18:59:45 +0100

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+function openModal(id) {
+  document.getElementById(id).style.display = "block";
+}
+
+function closeModal(id) {
+  document.getElementById(id).style.display = "none";
+}
+
+// Fermer la modale si on clique en dehors
+window.onclick = function(event) {
+  const modals = document.querySelectorAll(".modal");
+  modals.forEach(modal => {
+    if (event.target === modal) {
+      modal.style.display = "none";
+    }
+  });
+};
+
